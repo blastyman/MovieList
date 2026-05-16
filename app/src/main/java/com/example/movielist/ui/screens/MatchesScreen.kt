e8875441bd74008e89f58d6fa18f04113bd1d47a
bl4stySKID <gamwtimana69@gmail.com>
2026-05-16 16:28:24 +0300

    feat: optimize imports

--- app/src/main/java/com/example/movielist/ui/screens/MatchesScreen.kt
+++ app/src/main/java/com/example/movielist/ui/screens/MatchesScreen.kt
@@ -1,9 +1,19 @@
 package com.example.movielist.ui.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
